b35a785511c71d13a8fcb96ca2315bee50d34a03
lkroll <24440161+lkroll@users.noreply.github.com>
2022-10-19 11:34:30 +0200

    3.2.1 Added class Kreis

---
+++ Kreis.java
@@ -0,0 +1,11 @@
+public class Kreis {
+
+    public static void main(String[] args) {
+
+        // 3.2.1 Kreis-Ausgabe
+        System.out.printf("%5s\n","**");
+        System.out.printf("%s%7s\n",'*','*');
+        System.out.printf("%s%7s\n",'*','*');
+        System.out.printf("%5s\n","**");
+    }
+}
