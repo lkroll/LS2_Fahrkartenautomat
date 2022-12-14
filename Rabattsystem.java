e998b851f108a832ab0487ad8bdd176d471c7903
lkroll <24440161+lkroll@users.noreply.github.com>
2022-12-14 10:49:57 +0100

    4.7.1 Rabattsystem

---
+++ Rabattsystem.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class Rabattsystem {
+
+    public static void main(String[] args) {
+
+        double bestellwert;
+
+        Scanner tastatur = new Scanner(System.in);
+
+        System.out.print("Bitte geben Sie Ihren Bestellwert ein: ");
+        bestellwert = tastatur.nextDouble();
+
+        if (bestellwert >= 0.0 && bestellwert <= 100.0) {
+            bestellwert = bestellwert * 0.9;
+        }
+
+        else if (bestellwert > 100.0 && bestellwert <= 500.0) {
+            bestellwert = bestellwert * 0.85;
+        }
+
+        else {
+            bestellwert = bestellwert * 0.8;
+        }
+
+        System.out.println("Der Bestellwert abzüglich Rabatt beträgt: " + bestellwert);
+    }
+}
